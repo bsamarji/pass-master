143e4287eb712ceab861b4a0e082e05e3d0bdb6a
bsamarji <bensamarji5637@gmail.com>
2025-12-02 16:49:46 +0000

    Updated the PEK global variable in conftest to be exactly 32 bytes

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,7 @@ from keepr.db import get_db_connection, initialise_db
 from keepr.internal_config import APP_DIR_NAME, DB_FILE_NAME
 
 # --- Define a temporary key for testing ---
-TEST_PEK_HEX = "0102030405060708090a0b0c0d0e0f10"
-TEST_PEK = bytes.fromhex(TEST_PEK_HEX)
+TEST_PEK = b"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20"
 
 
 # Fixture to mock the database path setup
